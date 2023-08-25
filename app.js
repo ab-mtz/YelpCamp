b914ba29311466c931cad68316d1a6f07f8b7c87
abrahamMartinez <bmtzs@me.com>
2023-08-25 14:07:32 +0200

    asdf

--- app.js
+++ app.js
@@ -5,34 +5,32 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
 //I had an error trying to conect trough this path: localhost:27017
-mongoose.connect('mongodb://127.0.0.1/yelp-camp', {
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
 //Next lines have been deprecated
     // useNewUrlParser: true,
     // // useCreateIndex: true,
     // useUnifiedTopology: true
 });
 
-const db = connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "DB conection error:"));
 db.once("open", () => {
     console.log("Database conected");
 })
 
 
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))  //when we want request info of the body to post
-app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
-    res.render('Yelp Camp Home')
+    res.render('home')
 })
 
 
 app.get('/campgrounds', async (req, res) =>{
-   const campgrounds = await Campgrounds.find({});
+   const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 })
 
