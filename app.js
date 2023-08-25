f8fe4f18bf33ad593522da5322dc29a818cba7f5
abrahamMartinez <bmtzs@me.com>
2023-08-25 09:53:17 +0200

    db connection issue resolved

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//I had an error trying to conect trough this path: localhost:27017
+mongoose.connect('mongodb://127.0.0.1/yelp-camp', {
 //Next lines have been deprecated
     // useNewUrlParser: true,
     // // useCreateIndex: true,
     // useUnifiedTopology: true
 });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Conection error:"));
 db.once("open", () => {
     console.log("Database conected");
