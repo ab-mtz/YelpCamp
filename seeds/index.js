f64238532c0019b3eab73d941ee2266e7d2cb3fd
abrahamMartinez <bmtzs@me.com>
2023-08-25 12:39:31 +0200

    asdf

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,9 @@
-
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
-const Campground = require('./models/campground');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+
 
 //I had an error trying to conect trough this path: localhost:27017
 mongoose.connect('mongodb://localhost:27017/', {});
@@ -17,7 +18,7 @@ const sample = array => Math.floor(Math.random() * 1000);
 
 
 const seedDB = async () => {
-    await Campground.insertMany({});
+    // await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
