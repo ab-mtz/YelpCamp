bebbea83312d2557a03fa4e92ee5fa074eca8601
ab-mtz <bmtzs@me.com>
2023-09-11 20:53:32 +0200

    rev

--- app.js
+++ app.js
@@ -91,12 +91,13 @@ app.put('/campgrounds/:id', catchAsync(async (req, res) => {
 }))
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    console.log("Trying to delete")
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }))
 
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) =>{
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
     const {id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId}});
     await Review-Review.findByIdAndDelete(reviewId);
@@ -104,6 +105,7 @@ app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) =>{
 }))
 
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
+    console.log(req.params)
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
