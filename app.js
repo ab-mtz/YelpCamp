d96e47f570103d1532a64b9c4eef1ea2ece5c632
ab-mtz <bmtzs@me.com>
2023-09-11 19:33:32 +0200

    review val works

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Joi = require('joi')
-const { campgroundSchema, reviewSchema } = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -41,19 +41,17 @@ const validateCampground = (req, res, next) => {
     } else {
         next();
     }
-    console.log(result)
 }
 
 // Middleware to reviews
 const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
+    const { error } = reviewSchema.validate(req.body);
     if(error){
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else {
         next();
     }
-    console.log(result)
 }
 
 app.get('/', (req, res) => {
@@ -71,8 +69,6 @@ app.get('/campgrounds/new', (req, res) => {
 
  app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
-    
-    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
